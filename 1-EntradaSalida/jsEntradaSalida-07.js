f1d56f288432a7cd1df219cbff9620c41b6e39c2
fwaicen <fwaicen@gmail.com>
2020-07-18 20:32:48 -0300

    Fix Ejercicio 7

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -4,24 +4,34 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 
-function sumar(num1, num2)
+function sumar()
 {	
+	const { num1, num2 } = obtenerNumeros();
 	alert("La Suma es "+ (num1 + num2));	
 }
 
-function restar(num1, num2)
+function restar()
 {
+	const { num1, num2 } = obtenerNumeros();
 	alert("La Resta es "+ (num1 - num2));
 }
 
-function multiplicar(num1, num2)
+function multiplicar()
 { 
+	const { num1, num2 } = obtenerNumeros();
 	alert("La Multiplicación es "+ (num1 * num2));
 }
 
-function dividir(num1, num2)
+function dividir()
 {
-	alert("La División es "+ (num1 / num2));
+	const { num1, num2 } = obtenerNumeros();
+	alert("La División es "+ (num1 / num2).toFixed(2));
 }
 
+function obtenerNumeros()
+{
+	const num1 = document.getElementById('txtIdNumeroUno').value;
+	const num2 = document.getElementById('txtIdNumeroDos').value;
+	return { num1:parseFloat(num1), num2:parseFloat(num2) };
+}
 
